d44b1001a73428bd8b42cbe3d3e9374e59a9fe77
Wayne Yong <ymw0331@gmail.com>
2023-08-19 17:15:28 +0800

    add buit in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,12 @@ export class AppComponent {
 
   // courses array
   courses = COURSES;
+  title = COURSES[0].description;
+  price = 9.99;
+  rate = 0.67;
+  course = COURSES[0]
+
+  startDate = new Date(2000, 0, 1)
 
   // receive input from eventEmitter from courseCardClicked
   onCourseSelected(course: Course) {
