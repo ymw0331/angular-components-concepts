ccd108d6dcb7908d2f240328eccd5ac124d670cd
Wayne Yong <ymw0331@gmail.com>
2023-08-22 00:23:27 +0800

    add ng content child decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { CourseImageComponent } from './course-image/course-image.component';
 
 @Component({
   selector: 'app-root',
@@ -20,19 +21,19 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('courseImage')
   courseImage: CourseCardComponent;
 
-  @ViewChild('container')
-  containerDiv: ElementRef;
+  // query by element or native dom element or component instance
+  @ViewChild(CourseImageComponent)
+  image: CourseImageComponent;
   // used to query plain dom element, or component
 
   constructor() {
-    console.log("containerDiv", this.containerDiv)
 
   }
 
   // ng method to call after all child are filled in, called by ng (lifecycle hook)
   ngAfterViewInit(): void {
     // console.log("ngafterViewInit(): AppComponent", this.card1)
-    console.log('courseImage', this.courseImage)
+    console.log('courseImage', this.image)
 
 
     // this.courses[0].description = 'test'
