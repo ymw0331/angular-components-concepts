94c0edc13bccc144ff7458bbb2fabc67f677131d
Wayne Yong <ymw0331@gmail.com>
2023-09-09 15:50:25 +0800

    add section5/ngTemplates-intro

--- src/db-data.ts
+++ src/db-data.ts
@@ -1,8 +1,8 @@
 export const COURSES: any = [
     {
         id: 1,
-        description: "Angular Core Deep dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        description: "Angular Core Deep Dive",
+        // iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
         longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
         lessonsCount: 10
     },
